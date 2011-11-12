996e4a87cfb024bc124ed207895d51b930d57bc9
leitesgustavo@gmail.com <leitesgustavo@gmail.com@2abaf727-5b33-aaa4-437b-b207061e33ab>
2011-11-12 15:02:17 +0000

    First commit from python qrDecoded proyect

---
+++ QRDecodedApp/src/xmlRpcCom/RpcClient.py
@@ -0,0 +1,17 @@
+'''
+Created on 10/11/2011
+
+@author: gustavo
+'''
+
+class RpcClient(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
