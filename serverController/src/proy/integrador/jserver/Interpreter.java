6104b517c343ac4ad0ad2b9662d652cea2d8512a
pablodmv@gmail.com <pablodmv@gmail.com@2abaf727-5b33-aaa4-437b-b207061e33ab>
2011-11-17 22:53:25 +0000

    controladora

---
+++ serverController/src/proy/integrador/jserver/Interpreter.java
@@ -0,0 +1,27 @@
+package proy.integrador.jserver;
+
+public class Interpreter {
+	
+	private String data;
+	
+	public Interpreter(){
+		
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public String readData(String data){
+		this.data = data;
+		System.out.println("Obtengo el mensaje desde python: " + data);
+                //TODO: COntrolar formato del mensaje que se recibe
+		return data;
+	}
+	
+
+}
