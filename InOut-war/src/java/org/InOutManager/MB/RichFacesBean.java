13f2e70043b1228b5fd9092e87a52c0e537d0a8c
pablodmv@gmail.com <pablodmv@gmail.com@2abaf727-5b33-aaa4-437b-b207061e33ab>
2011-11-17 23:32:28 +0000

    Esqueleto inicial InOut

---
+++ InOut-war/src/java/org/InOutManager/MB/RichFacesBean.java
@@ -0,0 +1,30 @@
+/*
+ * __NAME__.java
+ *
+ * Created on 17 de noviembre de 2011, 09:25 PM
+ */
+ 
+package org.InOutManager.MB;           
+
+/** 
+ *
+ * @author pablo
+ * @version 
+ */
+
+public class RichFacesBean {
+
+    private String text;
+
+    public RichFacesBean() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
