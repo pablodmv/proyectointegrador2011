6dedbe3fa587b3583dbc35a1017db010924f13fd
pablodmv@gmail.com <pablodmv@gmail.com@2abaf727-5b33-aaa4-437b-b207061e33ab>
2011-11-22 00:11:04 +0000

    Webservice desde ejb

---
+++ InOutManager/InOutManager-ejb/src/java/com/inout/ejb/tarjeta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.inout.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author pablo
+ */
+@Stateless
+public class tarjeta implements tarjetaLocal {
+    
+   //public Boolean altaTarjeta(String id, Integer tipo, )
+ 
+}
