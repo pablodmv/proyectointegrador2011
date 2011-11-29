7e907ceb4391b5760424f669fde8400425f2c11e
leitesgustavo@gmail.com <leitesgustavo@gmail.com@2abaf727-5b33-aaa4-437b-b207061e33ab>
2011-11-29 02:16:33 +0000

    InOutPF-ejb update 27 Nov 22:28

---
+++ InOutManager-EAR/InOutManager-war/src/java/ati/manager/inout/beans/MenuBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ati.manager.inout.beans;
+
+
+/**
+ *
+ * @author gustavo
+ */
+public class MenuBean {
+
+    /** Creates a new instance of MenuBean */
+    public MenuBean() {
+    }
+
+
+    public String goVerMarcas(){
+        return "verMarcas";
+    }
+
+    public String goAltaPersona(){
+        return "altaPersona";
+    }
+
+    public String goEditPersona(){
+        return "editarPersona";
+    }
+
+}
