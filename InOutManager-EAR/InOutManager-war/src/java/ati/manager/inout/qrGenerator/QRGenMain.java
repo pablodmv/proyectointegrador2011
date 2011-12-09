b1e4fb5b858d7c715634ac088051288a47e38bec
leitesgustavo@gmail.com <leitesgustavo@gmail.com@2abaf727-5b33-aaa4-437b-b207061e33ab>
2011-12-09 02:13:38 +0000

    InOutManajer-war update 09 Dic 00:13

---
+++ InOutManager-EAR/InOutManager-war/src/java/ati/manager/inout/qrGenerator/QRGenMain.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ati.manager.inout.qrGenerator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Writer;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author gustavo
+ */
+public class QRGenMain {
+
+    private static final String FORMATO_IMAGEN = "gif";
+    private static final String RUTA_IMAGEN = System.getProperty("user.home") + "/qrZxing.gif";
+    private static final int ancho = 300;
+    private static final int alto = 300;
+    private static String datos = "http://monillo007.blogspot.com";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        {
+            FileOutputStream qrCode = null;
+            try {
+                BitMatrix bm;
+                Writer writer = new QRCodeWriter();
+                bm = writer.encode(datos, BarcodeFormat.QR_CODE, ancho, alto);
+                BufferedImage image = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_RGB);
+                for (int y = 0; y < ancho; y++) {
+                    for (int x = 0; x < alto; x++) {
+                        int grayValue = (bm.get(x, y) ? 1 : 0) & 0xff;
+                        image.setRGB(x, y, grayValue == 0 ? 0 : 0xFFFFFF);
+                    }
+                }
+                image = invertirColores(image);
+                qrCode = new FileOutputStream(RUTA_IMAGEN);
+                ImageIO.write(image, FORMATO_IMAGEN, qrCode);
+            } catch (IOException ex) {
+                Logger.getLogger(QRGenMain.class.getName()).log(Level.SEVERE, null, ex);
+            
+            } catch (WriterException ex) {
+                Logger.getLogger(QRGenMain.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    qrCode.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(QRGenMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            }
+
+
+            Desktop d = Desktop.getDesktop();
+            try {
+                d.open(new File(RUTA_IMAGEN));
+                System.out.println(RUTA_IMAGEN);
+            } catch (IOException ex) {
+                Logger.getLogger(QRGenMain.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
+
+    private static  BufferedImage invertirColores(BufferedImage imagen) {
+        for (int x = 0; x < ancho; x++) {
+            for (int y = 0; y < alto; y++) {
+                int rgb = imagen.getRGB(x, y);
+                if (rgb == -16777216) {
+                    imagen.setRGB(x, y, -1);
+                } else {
+                    imagen.setRGB(x, y, -16777216);
+                }
+            }
+        }
+        return imagen;
+    } 
+
+
+}
